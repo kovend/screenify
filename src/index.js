0a0e975454715334d2a53379762ba00d6b8080f9
Kovendan Ragupathi <kovendankumar@gmail.com>
2023-03-07 21:26:15 +0530

    NavBar section done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import App from './components/App';
 
+const theme = createTheme({});
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+    ,
   </React.StrictMode>,
   document.getElementById('root'),
 );
